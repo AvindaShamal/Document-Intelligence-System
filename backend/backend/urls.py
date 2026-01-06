da46c002c58cfa34cd5712c2088339743af5f031
AvindaShamal <avindashamal@gmail.com>
2026-01-06 14:01:41 +0530

    Add documents app with initial configuration and media settings

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-    path("api/auth", include("accounts.urls")),
+    path("api/auth/", include("accounts.urls")),
 ]
 
 urlpatterns += [
     path("api/auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
